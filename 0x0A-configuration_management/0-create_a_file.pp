75a97d24d3b649a2d1cb3f8c97e2a14463e12fb1
Suitret <suitret01@gmail.com>
2023-08-25 15:16:34 -0700

    0. Create a file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Puppet manifest to create a file in /tmp with specific attributes
+
+file { '/tmp/school':
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
