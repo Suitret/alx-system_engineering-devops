ae208ff26bd9cd7e87f35711137b11668dd0b4df
Suitret <suitret01@gmail.com>
2024-09-02 12:23:18 -0700

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""Module to give the count of subreddits
+"""Module to give the count of subscriber for a subreddit
 """
 import requests
-from requests.auth import HTTPBasicAuth
-
-
-client_id = 'iABR_GeVfRaPZJJDIPcXng'
-client_secret = 'x30M5_WNFaH7EWSRFswOwMhab5cqFg'
 
 
 def number_of_subscribers(subreddit):
-    """Set up the API URL for the subreddit's information
+    """Function to give the count of subscriber for a subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
@@ -20,20 +15,11 @@ def number_of_subscribers(subreddit):
         'python:subreddit.subscriber.counter:v1.0.0 (by /u/Mental_Way_8071)'
     }
 
-    try:
-        # Make a GET request to the Reddit API
-        response = requests.get(url, headers=headers,
-                                auth=HTTPBasicAuth(client_id, client_secret),
-                                allow_redirects=False)
+    # Make a GET request to the Reddit API
+    response = requests.get(url, headers=headers,
+                            allow_redirects=False)
 
-        # Check if the status code is 200 (OK)
-        if response.status_code == 200:
-            # Parse the JSON response
-            data = response.json()
-            # Return the number of subscribers
-            return data['data']['subscribers']
-        else:
-            return 0
-
-    except requests.RequestException as e:
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
