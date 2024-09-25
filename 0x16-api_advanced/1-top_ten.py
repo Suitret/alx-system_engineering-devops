99fc8ffbfd63e336fb43997fb311abd38c593d40
Suitret <suitret01@gmail.com>
2024-09-25 13:07:48 -0700

    1. Top Ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,7 +8,7 @@ import requests
 def top_ten(subreddit):
     """prints the titles of the first 10 hot posts
     listed for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     agent = 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:124.0)'
     agent += ' Gecko/20100101 Firefox/124.0'
     headers = {'User-Agent': agent}
